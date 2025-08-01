7c82f7a9f8d129db51a11445fb9eb60c9a62cf6b
Harry Pujols <harrypujols@gmail.com>
2025-08-01 10:59:30 -0400

    reorganizing imports

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,9 +2,9 @@ const FRAMEWORK = {};
 
 import breakpoint from "./functions/breakpoint.js";
 import resizestop from "./functions/resizestop.js";
-import test from "./components/test.js";
 import components from "./app/components.js";
 import run from "./app/run.js";
+import test from "./components/test.js";
 
 ((window, APP) => {
   APP.methods = {
